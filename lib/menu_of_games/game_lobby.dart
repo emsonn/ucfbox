e0d0866503378c5790656dc1b4f6b962d4d060fd
Da Kim <kellydkim@gmail.com>
2019-11-10 22:29:34 -0500

    deleted useless game files

--- lib/menu_of_games/game_lobby.dart
+++ lib/menu_of_games/game_lobby.dart
@@ -1,12 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_swiper/flutter_swiper.dart';
-
-// import 'package:ucfbox/games/citronot.dart';
-// import 'package:ucfbox/games/night_night_knightro.dart';
-// import 'package:ucfbox/games/quiplash.dart';
 import 'package:ucfbox/my_app_bar.dart';
 import 'game_card.dart';
-
 
 //Create the ROUTING SYSTEM to be able to move to game.
 class GameLobby extends StatelessWidget {
@@ -14,17 +9,17 @@ class GameLobby extends StatelessWidget {
     GameCard(
       label: "CITRO\nNOT",
       color: Color(0xFFF10429),
-  //   route: Citronot(),
+      //   route: Citronot(),
     ),
     GameCard(
       label: "QUIP\nLASH",
       color: Color(0xFF3DDA03),
-   //   route: Quiplash(),
+      //   route: Quiplash(),
     ),
     GameCard(
       label: "NIGHT\nNIGHT\nKNIGHTRO",
       color: Color(0xFFBE038B),
-   //   route: NightNightKnightro(),
+      //   route: NightNightKnightro(),
     ),
   ];
 
