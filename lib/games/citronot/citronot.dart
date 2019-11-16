cf1569a6148a53fd3eb46a0c68f990f4f9001f30
ECLKnight <pepi91cl@gmail.com>
2019-11-16 01:45:05 -0500

    Doing UI

--- lib/games/citronot/citronot.dart
+++ lib/games/citronot/citronot.dart
@@ -1,38 +1,30 @@
-import 'dart:ui' as prefix0;
-
 import 'package:flutter/material.dart';
 import 'package:ucfbox/my_app_bar.dart';
 import 'package:ucfbox/games/citronot/howtoplay.dart';
 
-void CNGame() => runApp(Citronot());
-
-class Citronot extends StatelessWidget {
+class Citronot extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return new MaterialApp(home: CitronotHomeScreen());
-  }
+  _CitronotState createState() => _CitronotState();
 }
 
-class CitronotHomeScreen extends StatelessWidget {
+class _CitronotState extends State<Citronot> {
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
       backgroundColor: Color(0xFFFFC904),
       appBar: MyAppBar(),
-//
-//        )),
       body: SafeArea(
         child: Column(
           children: <Widget>[
+            /// Logo
             Expanded(
-              flex: 0,
+              flex: 5,
               child: Image.asset(
                 'images/citronot.png',
               ),
             ),
-//              SizedBox(
-//                height: 20,
-//              ),
+
+            /// List of players
             Expanded(
               flex: 3,
               child: Text(
@@ -47,6 +39,8 @@ class CitronotHomeScreen extends StatelessWidget {
             SizedBox(
               height: 60,
             ),
+
+            /// How to play button
             Expanded(
               flex: 0,
               child: RaisedButton(
@@ -69,6 +63,7 @@ class CitronotHomeScreen extends StatelessWidget {
               height: 10,
             ),
 
+            /// Start Game Button
             Expanded(
               flex: 0,
               child: RaisedButton(
@@ -95,3 +90,90 @@ class CitronotHomeScreen extends StatelessWidget {
     );
   }
 }
+
+// class Citronot extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return new MaterialApp(home: CitronotHomeScreen());
+//   }
+// }
+
+// class CitronotHomeScreen extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return new Scaffold(
+//       backgroundColor: Color(0xFFFFC904),
+//       appBar: MyAppBar(),
+
+//       body: SafeArea(
+//         child: Column(
+//           children: <Widget>[
+//             Expanded(
+//               flex: 0,
+//               child: Image.asset(
+//                 'images/citronot.png',
+//               ),
+//             ),
+
+//             Expanded(
+//               flex: 3,
+//               child: Text(
+//                 'PLAYERS',
+//                 textAlign: TextAlign.center,
+//                 style: TextStyle(
+//                   fontSize: 25,
+//                   fontWeight: FontWeight.bold,
+//                 ),
+//               ),
+//             ),
+//             SizedBox(
+//               height: 60,
+//             ),
+//             Expanded(
+//               flex: 0,
+//               child: RaisedButton(
+//                 textColor: Color(0xFFFFC904),
+//                 color: Colors.black,
+//                 child: Text(
+//                   'How to Play',
+//                   style: TextStyle(
+//                     fontSize: 20,
+//                     fontWeight: FontWeight.bold,
+//                   ),
+//                 ),
+//                 onPressed: () {
+//                   Navigator.push(context,
+//                       MaterialPageRoute(builder: (context) => HowToPlay()));
+//                 },
+//               ),
+//             ),
+//             SizedBox(
+//               height: 10,
+//             ),
+
+//             Expanded(
+//               flex: 0,
+//               child: RaisedButton(
+//                 textColor: Color(0xFFFFC904),
+//                 color: Colors.black,
+//                 child: Text(
+//                   'Start Game',
+//                   style: TextStyle(
+//                     fontSize: 20,
+//                     fontWeight: FontWeight.bold,
+//                   ),
+//                 ),
+//                 onPressed: () {
+//                   print('Start Game button has been pressed');
+//                 },
+//               ),
+//             ),
+//             SizedBox(
+//               height: 30,
+//             ),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
---
+++ lib/games/citronot/question.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Question extends StatefulWidget {
+  @override
+  _QuestionState createState() => _QuestionState();
+}
+
+class _QuestionState extends State<Question> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
