92a397932b0e46909717f4b72dd1b9e55a3f5a69
ECLKnight <pepi91cl@gmail.com>
2019-11-16 01:18:10 -0500

    Have to work with the nestin of answer

--- lib/models/game_rooms/knightquips_room.dart
+++ lib/models/game_rooms/knightquips_room.dart
@@ -1,14 +1,18 @@
+/// THE TYPES OF LIST HAVE TO CHANGE FOR
+/// ALLTOPICS AND ANSWER
 import 'package:firebase_database/firebase_database.dart';
+import 'package:ucfbox/games/knightquips/knightquips.dart';
 import 'package:ucfbox/models/players/knightquips_player.dart';
+
 class KnightQuipsRoom {
   String key;
   int noOfPlayers;
   String gameType;
   List<KnightQuipsPlayer> players;
   int voteCount;
-  List allTopics;
+  List<KnightQuipsPlayer> allTopics;
   String topic;
-  List answers;
+  List<KnightQuipsPlayer> answers;
 
   KnightQuipsRoom(
       {this.key,
@@ -16,17 +20,22 @@ class KnightQuipsRoom {
       this.gameType,
       this.players,
       this.voteCount,
-      this.allTopics,
-      this.topic,
       this.answers});
 
   KnightQuipsRoom.fromSnapshot(DataSnapshot snapshot)
-      : key = snapshot.value['key'],
+      : key = snapshot.key,
         noOfPlayers = snapshot.value['noOfPlayers'],
         gameType = snapshot.value['gameType'],
-        players = snapshot.value['players'],
+        players = (snapshot.value['players'] as List).map((i) {
+          return KnightQuipsPlayer.fromJson(i);
+        }).toList(),
         voteCount = snapshot.value['voteCount'],
-        allTopics = snapshot.value['allTopics'],
-        topic = snapshot.value['topic'],
-        answers = snapshot.value['answers'];
+
+        /// Need to change the TYPE of list
+        allTopics = (snapshot.value['allTopics'] as List).map((i) {
+          return KnightQuipsPlayer.fromJson(i);
+        }).toList(),
+        answers = (snapshot.value['allTopics'] as List).map((i) {
+          return KnightQuipsPlayer.fromJson(i);
+        }).toList();
 }
