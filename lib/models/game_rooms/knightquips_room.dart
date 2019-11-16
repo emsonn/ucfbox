bc4bed9bff8367d6440e70ef2483f554f6d31ba1
ECLKnight <pepi91cl@gmail.com>
2019-11-15 23:54:23 -0500

    commited to this group

--- lib/models/game_rooms/knightquips_room.dart
+++ lib/models/game_rooms/knightquips_room.dart
@@ -1,16 +1,18 @@
 import 'package:firebase_database/firebase_database.dart';
-
+import 'package:ucfbox/models/players/knightquips_player.dart';
 class KnightQuipsRoom {
+  String key;
   int noOfPlayers;
   String gameType;
-  List players;
+  List<KnightQuipsPlayer> players;
   int voteCount;
   List allTopics;
   String topic;
   List answers;
 
   KnightQuipsRoom(
-      {this.noOfPlayers,
+      {this.key,
+      this.noOfPlayers,
       this.gameType,
       this.players,
       this.voteCount,
@@ -19,24 +21,12 @@ class KnightQuipsRoom {
       this.answers});
 
   KnightQuipsRoom.fromSnapshot(DataSnapshot snapshot)
-      : noOfPlayers = snapshot.value['noOfPlayers'],
+      : key = snapshot.value['key'],
+        noOfPlayers = snapshot.value['noOfPlayers'],
         gameType = snapshot.value['gameType'],
         players = snapshot.value['players'],
         voteCount = snapshot.value['voteCount'],
         allTopics = snapshot.value['allTopics'],
         topic = snapshot.value['topic'],
         answers = snapshot.value['answers'];
-
-
-        toJson(){
-          return {
-            'noOfPlayers': noOfPlayers,
-            'gameType': gameType,
-            'players': players,
-            'voteCount': voteCount,
-            'allTopics': allTopics,
-            'topic': topic,
-            'answers': answers
-          };
-        }
 }
