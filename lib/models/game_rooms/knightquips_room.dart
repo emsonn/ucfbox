6ec1079c6e5829d67341aade24cad11564f5f73a
ECLKnight <pepi91cl@gmail.com>
2019-11-20 14:51:29 -0500

    Comments on knightquips_room.dart

--- lib/models/game_rooms/knightquips_room.dart
+++ lib/models/game_rooms/knightquips_room.dart
@@ -9,7 +9,35 @@ class KnightQuipsRoom {
   int noOfPlayers;
   String gameType;
   List<KnightQuipsPlayer> players;
+/// Moving the answer and vote to the player object would eliminate
+/// the nesting in "allTopics".
+/// The answer and votes will always be re-writen.
+///
+/// Since they are so close to the player object itself I find it 
+/// that it makes sense for this game in particular.
+/// 
+/// By doing so we eliminate most of allTopics nesting. The thing that 
+/// we would have inside is just a list of the topics that we already
+/// played. 
+/// 
+/// "players": [
+///       {
+///           "playersName": Emanuel,
+///           "score": 1,
+///           "votes": 3,
+///           "answer": Do work,
+///           "start": true,
+///       }
+/// ],
+/// "voteCount": 5,
+/// "allTopics": [
+///       {
+///           "topic": topic1,
+///           "topic": topic2,
+///       }
+/// ]
   int voteCount;
+  
   List<KnightQuipsPlayer> allTopics;
   String topic;
   List<KnightQuipsPlayer> answers;
